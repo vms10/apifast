cc7850ceb7585b6e35dd35754802f4b90a1faa8b
vms10 <msolvidal10@hotmail.com>
2025-10-18 13:05:10 -0300

    avances clases

---
+++ README.md
@@ -0,0 +1,4 @@
+Con el comando  fastapi dev main.py levantamos el servidor
+Si lo probas localmente: url + nombre del endpoint (hola)
+http://127.0.0.1:8000/hola
+Ahi te devuelve el mensaje
--- main.py
+++ main.py
@@ -1,1 +1,68 @@
 # archivo principal
+import os
+from fastapi import FastAPI, File, UploadFile
+from typing import Annotated
+app = FastAPI()
+
+@app.get("/hola")
+async def prueba_root():
+    return {"message": "Accediste al endpoint de prueba"}
+
+@app.get("/files")
+async def buscar_archivos():
+    carpeta = "files"
+
+    # Verifica que la carpeta exista
+    if not os.path.exists(carpeta):
+        return {"error": f"La carpeta '{carpeta}' no existe."}
+
+    # Lista solo los archivos dentro de la carpeta
+    archivos = [
+        f for f in os.listdir(carpeta)
+        if os.path.isfile(os.path.join(carpeta, f))
+    ]
+    return {"archivos": archivos}
+
+
+
+
+
+
+# Endpoint que guarda el archivo en la carpeta "files"
+@app.post("/files")
+async def upload_file(
+    file: Annotated[UploadFile, File(...)]
+):
+    # Asegurarte de que exista la carpeta "files"
+    os.makedirs("files", exist_ok=True)
+
+    # Ruta completa donde se va a guardar
+    save_path = os.path.join("files", os.path.basename(file.filename))
+
+    # Leer el contenido del archivo y escribirlo
+    with open(save_path, "wb") as f:
+        content = await file.read()
+        f.write(content)
+
+    return {"message": f"Archivo guardado como {save_path}", "size": len(content)}
+
+
+@app.get("/files/{file_name}")
+async def contenido_archivo(file_name):
+    carpeta = "files"
+    
+    # Verifica que la carpeta exista
+    if not os.path.exists(carpeta):
+        return {"error": f"La carpeta '{carpeta}' no existe."}
+
+    # Lista solo los archivos dentro de la carpeta
+    archivos = [
+        f for f in os.listdir(carpeta)
+        if os.path.isfile(os.path.join(carpeta, file_name))
+    ]
+    return {"archivos": os.path.join(carpeta, file_name)}
+
+
+
+
+
