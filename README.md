0bbbd6e64987980113c77a6cd7b2c52f43be2866
vms10 <msolvidal10@hotmail.com>
2025-10-22 20:16:23 -0300

    avances casa

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ Con el comando  fastapi dev main.py levantamos el servidor
 Si lo probas localmente: url + nombre del endpoint (hola)
 http://127.0.0.1:8000/hola
 Ahi te devuelve el mensaje
+
+
+desde un google si pones:
+http://127.0.0.1:8000/files/prueba45.txt
+
+te devuelve el contenido de ese archivo
+
+####################################
+Desde la terminar en Windows el comando es: 
+curl.exe -X GET "http://127.0.0.1:8000/files"
+ curl.exe -X 'GET' 'http://127.0.0.1:8000/files/prueba45.txt'
+
+curl.exe -X 'POST' 'http://127.0.0.1:8000/files' -H 'Content-Type: application/json' -d '{"name": "test.txt", "content": "Este es un archivo de prueba"}' 
+
+
+
+
+Para crear el requirements.txt
+ pip list
+ pip freeze > requirements.txt
+ 
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # archivo principal
 import os
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import PlainTextResponse
 from typing import Annotated
 app = FastAPI()
 
 @app.get("/hola")
 async def prueba_root():
     return {"message": "Accediste al endpoint de prueba"}
-
+####### 2.a) 
 @app.get("/files")
 async def buscar_archivos():
     carpeta = "files"
@@ -23,12 +24,7 @@ async def buscar_archivos():
     ]
     return {"archivos": archivos}
 
-
-
-
-
-
-# Endpoint que guarda el archivo en la carpeta "files"
+####### 2.b) 
 @app.post("/files")
 async def upload_file(
     file: Annotated[UploadFile, File(...)]
@@ -46,7 +42,7 @@ async def upload_file(
 
     return {"message": f"Archivo guardado como {save_path}", "size": len(content)}
 
-
+####### 2.c)
 @app.get("/files/{file_name}")
 async def contenido_archivo(file_name):
     carpeta = "files"
@@ -56,11 +52,14 @@ async def contenido_archivo(file_name):
         return {"error": f"La carpeta '{carpeta}' no existe."}
 
     # Lista solo los archivos dentro de la carpeta
-    archivos = [
-        f for f in os.listdir(carpeta)
-        if os.path.isfile(os.path.join(carpeta, file_name))
-    ]
-    return {"archivos": os.path.join(carpeta, file_name)}
+    archivo = os.path.join(carpeta, file_name)
+        # Lee y devuelve el contenido del archivo como texto
+    try:
+        with open(archivo, "r", encoding="utf-8") as f:
+            contenido = f.read()
+        return PlainTextResponse(content=contenido)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error al leer el archivo: {e}")
 
 
 
